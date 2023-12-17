13461c797e6ff354822b97d6aa7acc3b427a62cc
Sruthi Kallu <sruthi20152021@gmail.com>
2023-12-17 13:43:41 -0600

    roboshop shell scripts

---
+++ mongodb.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+
+echo "script stareted executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 ...$R FAILED $N"
+    else
+        echo -e "$2 ...$G SUCCESS $N"
+    fi
+
+}
+
+if [ $ID -ne 0 ]
+then
+    echo -e "$R ERROR: Please run this script with root acess $N"
+    exit 1 # you can give other than 0
+else
+    echo "you are root user"
+fi # fi means reverse of if, indicating condition end
+
+cp mongo.repo /etc/yum.repos.d/ &>> $LOGFILE
+
+VALIDATE $? "Copied MongoDB Repo"
